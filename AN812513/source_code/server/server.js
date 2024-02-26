38a2e9ce70c37da6d1397bb7a43dfc713e9f9d1d
Abhinav Chowdary <abhinavchowdary@Abhinavs-Air.lan>
2024-02-25 20:15:11 -0500

    saivng changes

---
+++ AN812513/source_code/server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 5001;
+app.use((req,res,next)=>
+{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
+    next();
+});
+app.use(bodyParser.json());
+
+console.log("resylt");
+
+// POST endpoint for addition operation
+app.post('/addition', (req, res) => {
+
+    
+    const { num1, num2 } = req.body;
+ 
+    const result = parseFloat(num1) + parseFloat(num2);
+    console.log(result);
+    res.json({ result })
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
