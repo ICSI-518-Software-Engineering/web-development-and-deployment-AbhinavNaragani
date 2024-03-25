22794de623b331c6a8421ba760b0acf9e15d55de
Abhinav Chowdary <abhinavchowdary@Abhinavs-Air.lan>
2024-03-24 23:37:04 -0400

    saving change

--- AN812513/source_code/server/server.js
+++ AN812513/source_code/server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require("path");
+const cors = require('cors');
 
 const app = express();
 const port = 5001;
-const path = require("path");
+
 app.use(express.static(path.join(__dirname,"public")));
 
 app.use((req,res,next)=>
@@ -14,6 +17,69 @@ app.use((req,res,next)=>
     next();
 });
 app.use(bodyParser.json());
+
+// Body parser middleware
+app.use(bodyParser.json({ limit:"10mb" }));
+
+// MongoDB connection
+mongoose.connect('mongodb+srv://abhinavn221:niEjK9Q1dXqca0o9@cluster0.moffxf2.mongodb.net/Cluster0')
+    .then(() => {
+        console.log('MongoDB connected');
+    })
+    .catch((error) => {
+        console.error('MongoDB connection error:', error);
+    });
+
+// Inventory Schema
+const inventorySchema = new mongoose.Schema({
+    name: String,
+    quantity: Number,
+    imageURL: String
+});
+
+// Inventory Model
+const InventoryItem = mongoose.model('InventoryItem', inventorySchema);
+
+// POST endpoint to add an inventory item
+app.get('/api/inventory', async (req, res) => {
+    try {
+        res.status(201).json(await InventoryItem.find({}));
+    } catch (error) {
+        console.error('Error adding item to inventory:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.post('/api/inventory', async (req, res) => {
+    try {
+        const newItem = new InventoryItem(req.body);
+        await newItem.save();
+        res.status(201).json(newItem);
+    } catch (error) {
+        console.error('Error adding item to inventory:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.put('/api/inventory/:id', async (req, res) => {
+    try {
+        const newItem = await InventoryItem.findByIdAndUpdate(req.params.id, req.body);
+        res.status(201).json(newItem);
+    } catch (error) {
+        console.error('Error adding item to inventory:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.delete('/api/inventory/:id', async (req, res) => {
+    try {
+        res.status(201).json(await InventoryItem.findByIdAndDelete(req.params.id));
+    } catch (error) {
+        console.error('Error adding item to inventory:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
 
 console.log("resylt");
 
@@ -31,3 +97,22 @@ app.post('/addition', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
